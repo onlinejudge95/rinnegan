432c82b087f258179e8a72ef2fcc11d15ea13a4d
onlinejudge95 <onlinejudge95@gmail.com>
2020-05-10 20:31:56 +0530

    Adds shell context processor

--- services/server/app/__init__.py
+++ services/server/app/__init__.py
@@ -7,6 +7,10 @@ def create_app(environemnt=None):
     app = Flask(__name__)
     app.config.from_object(cfg_map[environemnt])
 
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app}
+
     @app.route("/health")
     def index():
         return json.dumps({"health": "good"})
