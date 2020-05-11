05898e9f9ff0a5f912489a2985ca5ed97f844492
onlinejudge95 <onlinejudge95@gmail.com>
2020-05-11 22:33:44 +0530

    Fixes integration test for add user

--- services/server/app/tests/test_user_view.py
+++ services/server/app/tests/test_user_view.py
@@ -35,3 +35,63 @@ def test_add_user_empty_data(test_app, test_database):
 
     data = response.get_json()
     assert "Input payload validation failed" in data["message"]
+
+
+# Test user creation fails due to invalid data
+def test_add_user_empty_data(test_app, test_database):
+    client = test_app.test_client()
+    response = client.post(
+        "/users",
+        data=json.dumps({"email": "test_user@email.com"}),
+        headers={"Accept": "application/json", "Content-Type": "application/json"},
+    )
+
+    assert response.status_code == 400
+
+    data = response.get_json()
+    assert "Input payload validation failed" in data["message"]
+
+
+# Test user creation fails due to duplicate entry
+def test_add_user_duplicate_email(test_app, test_database):
+    client = test_app.test_client()
+    client.post(
+        "/users",
+        data=json.dumps(
+            {
+                "username": "test_user",
+                "email": "test_user@email.com",
+                "password": "test_password",
+            }
+        ),
+        headers={"Accept": "application/json", "Content-Type": "application/json"},
+    )
+    response = client.post(
+        "/users",
+        data=json.dumps(
+            {
+                "username": "test_user",
+                "email": "test_user@email.com",
+                "password": "test_password",
+            }
+        ),
+        headers={"Accept": "application/json", "Content-Type": "application/json"},
+    )
+    assert response.status_code == 400
+
+    data = response.get_json()
+    assert "test_user@email.com is already registered" in data["message"]
+
+
+# Test user creation fails due to invalid content-type header
+def test_add_user_empty_data(test_app, test_database):
+    client = test_app.test_client()
+    response = client.post(
+        "/users",
+        data=json.dumps({"email": "test_user@email.com"}),
+        headers={"Accept": "application/json"},
+    )
+    assert response.status_code == 415
+
+    data = response.get_json()
+    assert "define a Content-Type header" in data["message"]
