b13764272ac628a9091b18642f1cb0e5a49dba71
onlinejudge95 <onlinejudge95@gmail.com>
2020-05-12 12:19:53 +0530

    Refactors health check logic

--- services/server/app/api/health.py
+++ services/server/app/api/health.py
@@ -9,12 +9,13 @@ class Health(Resource):
     @health_namespace.response(200, "Health check passing")
     @health_namespace.response(404, "Health check failed")
     def get(self):
+        health = "bad"
         try:
             with open(app.config["HEALTHCHECK_FILE_PATH"], "r") as fp:
                 health = fp.read().strip()
                 return json.dumps({"health": health}), 200
         except IOError:
-            return json.dumps({"health": "bad"}), 404
+            return json.dumps({"health": health}), 404
 
 
 health_namespace.add_resource(Health, "")
