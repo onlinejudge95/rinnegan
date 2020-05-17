fdf619ea74258ad52fefa1f062afdb42a27469ef
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-05-18 01:17:31 +0530

    Adds User create form (#25)
    * Adds react forms
    
    * Updates .pep8speaks.yml

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,26 +2,58 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
 
 class App extends React.Component {
-  state = { users: [] };
+  state = { users: [], username: "", email: "", password: "" };
+
+  addUser = (event) => {
+    event.preventDefault();
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+
+    const data = {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    axios
+      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data, {
+        headers,
+      })
+      .then((response) => {
+        console.log(response);
+        this.getUsers();
+        this.setState({ username: "", email: "", password: "" });
+      })
+      .catch((err) => console.log(err));
+  };
 
   componentDidMount() {
     this.getUsers();
   }
 
-  getUsers() {
+  getUsers = () => {
+    const headers = { Accept: "application/json" };
+
     axios
-      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, {
-        headers: { Accept: "application/json" },
-      })
-      .then((res) => {
-        this.setState({ users: res.data });
+      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, { headers })
+      .then((response) => {
+        this.setState({ users: response.data });
       })
       .catch((err) => {
         console.log(err);
       });
-  }
+  };
+
+  handleChange = (event) => {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
 
   render() {
     return (
@@ -32,6 +64,15 @@ class App extends React.Component {
               <br />
               <h1 className="title is-1">Sentimental</h1>
               <hr />
+              <br />
+              <AddUser
+                addUser={this.addUser}
+                username={this.state.username}
+                email={this.state.email}
+                password={this.state.password}
+                handleChange={this.handleChange}
+              />
+              <br />
               <br />
               <UserList users={this.state.users} />
             </div>
