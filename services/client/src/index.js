4741c2e2064fdca61e8d430b712657c375dc8b91
onlinejudge95 <onlinejudge95>
2020-05-17 15:14:23 +0530

    Fixes CORS issue

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.getUsers();
+  }
+
+  getUsers() {
+    axios
+      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, {
+        headers: { Accept: "application/json" },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br />
+              <h1 className="title is-1">Sentimental</h1>
+              <hr />
+              <br />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- services/server/app/config.py
+++ services/server/app/config.py
@@ -12,7 +12,6 @@ class BaseConfig:
     ACCESS_TOKEN_EXPIRATION = 900
     REFRESH_TOKEN_EXPIRATION = 2592000
     JWT_ENCODE_ALGORITHM = "HS256"
-    CORS_ALLOWED_ORIGIN = os.getenv("CORS_ALLOWED_ORIGIN")
 
 
 class DevelopmentConfig(BaseConfig):
