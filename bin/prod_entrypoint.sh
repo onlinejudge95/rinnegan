f80a7acc3b0d77b0f09d0819988a43553da3ca57
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-05-23 17:56:06 +0530

    Adds production deployment support (#47)
    * Fixes deploy staging stage
    
    * Adds a production deploy stage
    
    * Adds dir creation for logs

--- bin/prod_entrypoint.sh
+++ bin/prod_entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+DIR="/var/log/gunicorn"
+
+if [ ! -d "$DIR" ]; then
+    echo "Info: ${DIR} does not exists. Creating\n"
+    mkdir -p ${DIR}
+else
+    echo "${DIR} exists\n"
+fi
+
+if [ $FLASK_ENV == "production" ]; then
+    flask db upgrade
+fi
+
 gunicorn --config /usr/src/app/gunicorn.conf.py manage:app && \
 sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && \
 nginx -g 'daemon off;'
