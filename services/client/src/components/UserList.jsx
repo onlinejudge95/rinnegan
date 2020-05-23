2b3bd484283d09cd33103df997923622862a5349
onlinejudge95 <onlinejudge95@gmail.com>
2020-05-23 12:29:21 +0530

    Refactor completed with adding modals

--- services/client/src/components/UserList.jsx
+++ services/client/src/components/UserList.jsx
@@ -4,19 +4,43 @@ import PropTypes from "prop-types";
 const UserList = (props) => {
   return (
     <div>
-      {props.users.map((user) => {
-        return (
-          <p key={user.id} className="box title is-4 username">
-            {user.username}
-          </p>
-        );
-      })}
+      <table className="table is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Username</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {props.users.map((user) => {
+            return (
+              <tr key={user.email}>
+                <td>{user.email}</td>
+                <td className="username">{user.username}</td>
+                {props.isAuthenticated() && (
+                  <td>
+                    <button
+                      className="button is-danger is-small"
+                      onClick={() => props.removeUser(user.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                )}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
 
 UserList.propTypes = {
   users: PropTypes.array.isRequired,
+  removeUser: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.func.isRequired,
 };
 
 export default UserList;
--- services/client/src/components/__tests__/UserList.test.jsx
+++ services/client/src/components/__tests__/UserList.test.jsx
@@ -5,18 +5,28 @@ import UserList from "../UserList";
 
 afterEach(cleanup);
 
-const users = [
+const userList = [
   { id: 1, username: "test_user_one", email: "test_user_one@mail.com" },
   { id: 2, username: "test_user_two", email: "test_user_two@mail.com" },
 ];
 
+const mockProps = {
+  users: userList,
+  removeUser: () => {
+    return true;
+  },
+  isAuthenticated: () => {
+    return true;
+  },
+};
+
 it("renders a username", () => {
-  const dom = render(<UserList users={users} />);
+  const dom = render(<UserList {...mockProps} />);
   expect(dom.getByText("test_user_one")).toHaveClass("username");
   expect(dom.getByText("test_user_two")).toHaveClass("username");
 });
 
 it("renders", () => {
-  const dom = render(<UserList users={users} />);
+  const dom = render(<UserList {...mockProps} />);
   expect(dom.asFragment()).toMatchSnapshot();
 });
