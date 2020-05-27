b178346969cdeece6ffa76f9d67554b76c468639
onlinejudge95 <onlinejudge95@gmail.com>
2020-05-27 19:43:41 +0530

    Uses multi stage build for client

--- services/client/entrypoint.sh
+++ services/client/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-
 sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf
-sed -i -e 's/$SERVER_URL_PLACEHOLDER/'"$REACT_APP_SERVER_URL"'/g' /etc/nginx/conf.d/default.conf
+sed -i -e 's~SERVER_URL_PLACEHOLDER~'"$REACT_APP_SERVER_URL"'~g' /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
