0c1470ad7720ce83b63166a679fac9d613a23b49
onlinejudge95 <onlinejudge95@gmail.com>
2020-05-28 21:37:23 +0530

    Adds actions for redux

---
+++ services/client/src/actions/types.js
@@ -0,0 +1,7 @@
+export const SIGN_UP_USER = "SIGN_UP_USER";
+export const SIGN_IN_USER = "SIGN_IN_USER";
+export const REFRESH_TOKEN = "REFRESH_TOKEN";
+export const GET_USERS = "GET_USERS";
+export const GET_USER = "GET_USER";
+export const REMOVE_USER = "REMOVE_USER";
+export const UPDATE_USER = "UPDATE_USER";
