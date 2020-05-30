02d59e3b3c53cb3bfaf8f91104635847a392cced
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-05-30 17:24:59 +0530

    Adds logging support in server (#56)
    * Adds logging config in server
    
    * Logging setup completed
    
    * Logging implemented in user and auth API
    
    * Fixes log level
    
    * Fixes failing tests
    
    * Removes failure on incorrect logging
    
    * Moves logging setup in manage.py

--- services/server/manage.py
+++ services/server/manage.py
@@ -1,6 +1,24 @@
 from app import create_app
 
+import coloredlogs
+import logging
+import logging.config
 import os
+import yaml
 
 
+def setup_logging():
+    with open("/usr/src/app/logging.yml", "r") as fp:
+        try:
+            config_dict = yaml.safe_load(fp.read())
+            logging.config.dictConfig(config_dict)
+            coloredlogs.install()
+        except Exception as e:
+            print(e)
+            print("There is an error in the logging configuration")
+        else:
+            logging.info("Logging setup finished successfully")
+
+
+setup_logging()
 app = create_app(os.getenv("FLASK_ENV"))
