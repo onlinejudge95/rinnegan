49023a81d1cd9c79b52036339bb4ed21e71f83d6
onlinejudge95 <onlinejudge95@gmail.com>
2020-06-06 20:16:09 +0530

    Adds Register & Login component

--- services/client/src/components/auth/LoginUser.jsx
+++ services/client/src/components/auth/LoginUser.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Button, Divider, Form, Input } from "semantic-ui-react";
 
 const LoginUser = () => {
-  return <div>LoginUser</div>;
+  return (
+    <main className="content">
+      <div className="ui container">
+        <div class="ui relaxed divided padded full grid">
+          <div class="row">
+            <div class="column">
+              <h1 class="ui header">
+                <div class="content">Sign-In</div>
+              </h1>
+            </div>
+          </div>
+          <Divider />
+          <div class="row">
+            <div class="eleven wide column">
+              <Form>
+                <Form.Field>
+                  <label>Email</label>
+                  <Input placeholder="Email" type="email" />
+                </Form.Field>
+                <Form.Field>
+                  <label>Password</label>
+                  <Input placeholder="Password" type="password" />
+                </Form.Field>
+                <Button type="submit" color="green">
+                  Submit
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default LoginUser;
