2ffe2643354a2f640374b35b8e03afccff1b887b
onlinejudge95 <onlinejudge95@gmail.com>
2020-06-07 12:55:27 +0530

    Renders ShowUser component on /profile route

--- services/client/src/components/user/ShowUser.jsx
+++ services/client/src/components/user/ShowUser.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import {
+  Divider,
+  Header,
+  Container,
+  Card,
+  Image,
+  Icon,
+  Button,
+} from "semantic-ui-react";
 
 const ShowUser = () => {
-  return <div>ShowUser</div>;
+  return (
+    <main className="content">
+      <Container>
+        <div className="ui relaxed divided padded full grid middle aligned">
+          <div className="row">
+            <div className="column">
+              <Header as="h1">Profile</Header>
+            </div>
+          </div>
+          <Divider />
+          <div className="row">
+            <div className="eight wide column">
+              <Card fluid>
+                <Image
+                  src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+                  wrapped
+                  ui={false}
+                />
+                <Card.Content>
+                  <Card.Header>Test Bot</Card.Header>
+                  <Card.Meta>
+                    <span className="email">Joined in 2020</span>
+                  </Card.Meta>
+                  <Card.Description>
+                    Test Bot is a programmer living in Bengaluru.
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Icon name="mail" />
+                  onlinejudge95@gmail.com
+                </Card.Content>
+              </Card>
+            </div>
+            <div className="eight wide column">
+              <Button.Group size="massive">
+                <Button massive positive fluid icon labelPosition="left">
+                  Edit
+                  <Icon name="edit" />
+                </Button>
+                <Button.Or />
+                <Button massive negative fluid icon labelPosition="right">
+                  Delete
+                  <Icon name="user delete" />
+                </Button>
+              </Button.Group>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </main>
+  );
 };
 
 export default ShowUser;
