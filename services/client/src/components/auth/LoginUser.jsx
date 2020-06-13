5a131f872c6178db30249a2749456bd71159b272
onlinejudge95 <onlinejudge95@gmail.com>
2020-06-13 16:45:20 +0530

    removes unnecessary comments

--- services/client/src/components/auth/LoginUser.jsx
+++ services/client/src/components/auth/LoginUser.jsx
@@ -86,37 +86,6 @@ const LoginUser = (props) => {
       </div>
     </div>
   );
-  // return (
-  //   <main className="content">
-  //     <Container>
-  //       <div className="ui relaxed divided padded full grid">
-  //         <div className="row">
-  //           <div className="column">
-  //             <Header as="h1">Sign-In</Header>
-  //           </div>
-  //         </div>
-  //         <Divider />
-  //         <div className="row">
-  //           <div className="eleven wide column">
-  //             <Form>
-  //               <Form.Field>
-  //                 <label>Email</label>
-  //                 <Input placeholder="Email" type="email" />
-  //               </Form.Field>
-  //               <Form.Field>
-  //                 <label>Password</label>
-  //                 <Input placeholder="Password" type="password" />
-  //               </Form.Field>
-  //               <Button type="submit" color="green">
-  //                 Submit
-  //               </Button>
-  //             </Form>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </Container>
-  //   </main>
-  // );
 };
 
 LoginUser.propTypes = {
