5daa8ad43500d251b5a7f26ec89a8431431e18b2
onlinejudge95 <onlinejudge95@gmail.com>
2020-07-13 19:41:53 +0530

    Refactors services/server/app/tests/conftest.py

--- services/server/app/tests/conftest.py
+++ services/server/app/tests/conftest.py
@@ -1,4 +1,4 @@
-import pytest
+from pytest import fixture
 
 from app import create_app
 from app import db
@@ -9,14 +9,14 @@ from app.api.users.crud import remove_user as remove_user_service
 from app.api.sentiment.crud import add_sentiment as add_sentiment_service
 
 
-@pytest.fixture(scope="function")
+@fixture(scope="function")
 def test_app():
     app = create_app("testing")
     with app.app_context():
         yield app
 
 
-@pytest.fixture(scope="function")
+@fixture(scope="function")
 def test_database():
     db.create_all()
 
@@ -26,7 +26,7 @@ def test_database():
     db.drop_all()
 
 
-@pytest.fixture(scope="function")
+@fixture(scope="function")
 def add_user():
     def _add_user(username, email, password):
         return add_user_service(username, email, password)
@@ -34,7 +34,7 @@ def add_user():
     return _add_user
 
 
-@pytest.fixture(scope="function")
+@fixture(scope="function")
 def remove_user():
     def _remove_user(user_id):
         user = get_user_by_id_service(user_id)
@@ -43,7 +43,7 @@ def remove_user():
     return _remove_user
 
 
-@pytest.fixture(scope="function")
+@fixture(scope="function")
 def login_user():
     def _login_user(user_id):
         return add_token_service(user_id)
@@ -51,7 +51,7 @@ def login_user():
     return _login_user
 
 
-@pytest.fixture(scope="function")
+@fixture(scope="function")
 def add_sentiments():
     def _add_sentiments(user_id, keyword):
         return add_sentiment_service(keyword, user_id)
